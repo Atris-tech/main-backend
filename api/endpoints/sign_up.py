fa8858bd780ac174b1c6b85b00e0d7b9ada78db3
Akshay Ashok <aks28id@gmail.com>
2020-12-19 13:06:12 +0530

    complete auth and user settings

--- api/endpoints/sign_up.py
+++ api/endpoints/sign_up.py
@@ -1,112 +1,88 @@
+from fastapi import Form
 from fastapi import APIRouter, HTTPException, BackgroundTasks
-from Services.auth.auth_services import sign_up, create_auth_url, check_user,\
-    update_user, token_check, create_verify_token
-from Services.mail.mail_service import send_mail
-from pydantic import EmailStr, BaseModel
-from starlette.requests import Request
-import error_constants
-
-
-class SignUpModel(BaseModel):
-    user_name: str
-    email: EmailStr
-    first_name: str
-    last_name: str
-    password: str
-
+from Services.auth_services import sign_up, create_auth_url, check_user, get_user_data, \
+    create_access_token, verify_access_token, update_user
+from Services.mail_service import send_mail
+from pydantic import EmailStr
 
 router = APIRouter()
 
 
 @router.post("/register/", status_code=200)
-def register(background_tasks: BackgroundTasks, register_obj: SignUpModel):
-    user_obj = sign_up(
-        user_name = register_obj.user_name.replace(" ", "").lower(),
-        email =register_obj.email.replace(" ", "").lower(),
-        first_name=register_obj.first_name.replace(" ", "").lower(),
-        last_name=register_obj.last_name.replace(" ", "").lower(),
-        password=register_obj.password
+def register(
+        background_tasks: BackgroundTasks,
+        user_name: str = Form(...),
+        email: EmailStr = Form(...),
+        first_name: str = Form(...),
+        last_name: str = Form(...),
+        password: str = Form(...)
+):
+    token = sign_up(
+        user_name, email, first_name, last_name,password=password 
     )
-
-    token_data = create_verify_token(user_obj)
-    token = token_data["token"]
-    url = create_auth_url(token, type="verify")
-    data = {"user": token_data["user_data"]["user_name"], "url": url}
-    background_tasks.add_task(send_mail, [str(register_obj.email)], "verify", data, "Verify your Atris Account")
-    return True
-
-
-class VerificationModel(BaseModel):
-    type: str
-    email: EmailStr
+    if token:
+        url = create_auth_url(token, type="verify")
+        data = {"user": user_name, "url": url}
+        background_tasks.add_task(send_mail, [str(email)], "verify", data)
+        return {
+            "access_token": token,
+            "token_type": "bearer",
+        }
+    else:
+        raise HTTPException(status_code=400, detail="User Name or Email does not exists")
 
 
 @router.post("/resend-verification/", status_code=200)
 def resend(
         background_tasks: BackgroundTasks,
-        verification_model_obj: VerificationModel
+        type: str = Form(...),
+        email: EmailStr = Form(...),
 ):
-    user_obj = check_user(email=verification_model_obj.email)
-    if user_obj:
-        token_data = create_verify_token(user_obj)
-        token = token_data["token"]
-        data = token_data["user_data"]
-        if verification_model_obj.type == "verify":
-            if user_obj.verified:
-                raise HTTPException(
-                    status_code=error_constants.EMAIL_ALREADY_VERIFIED["status_code"],
-                    detail=error_constants.EMAIL_ALREADY_VERIFIED["detail"]
-                )
+    if check_user(email=email):
+        data = get_user_data(email_address=email)
+        token = create_access_token(data=data)
+        if type == "verify":
             url = create_auth_url(token, type="verify")
             data = {"user": data["user_name"], "url": url}
-            background_tasks.add_task(send_mail, [str(verification_model_obj.email)], "verify", data,
-                                      "Verify your Atris Account")
+            background_tasks.add_task(send_mail, [str(email)], "verify", data)
             return True
-        elif verification_model_obj.type == "forgot_password":
+        elif type == "forgot_password":
             url = create_auth_url(token, type="forgot")
             data = {"user": data["user_name"], "url": url}
-            background_tasks.add_task(send_mail, [str(verification_model_obj.email)], "forgot", data,
-                                      "Reset your Atris Account")
+            background_tasks.add_task(send_mail, [str(email)], "forgot", data)
             return True
         else:
-            return HTTPException(status_code=400)
+            return HTTPException(status_code=400, detail="Bad Request")
     else:
-        raise HTTPException(
-            status_code=error_constants.INVALID_EMAIL["status_code"],
-            detail=error_constants.INVALID_EMAIL["detail"]
-        )
+        raise HTTPException(status_code=400, detail="Email does not exists")
 
 
 @router.post("/verify-user/", status_code=200)
 def verification(
-         request: Request
+        token: str = Form(...)
 ):
-    payload = token_check(request)
+    payload = verify_access_token(token)
     if not payload:
-        raise HTTPException(
-            status_code=error_constants.TOKEN_EXPIRED["status_code"],
-            detail=error_constants.TOKEN_EXPIRED["detail"]
-        )
+        raise HTTPException(status_code=401, detail="Token Expired")
     else:
-        verification_status = update_user(id=payload["id"], verified=True)
+        verification_status = update_user(email=payload["email"], verified=True)
         if verification_status:
             return True
         else:
-            raise HTTPException(status_code=400)
-
-
-class ForgotPasswordModel(BaseModel):
-    password: str
+            raise HTTPException(status_code=400, detail="Email does not exists")
 
 
 @router.post("/forgot-password/", status_code=200)
 def reset(
-        request: Request,
-        forgot_password_obj: ForgotPasswordModel
+        token: str = Form(...),
+        password: str = Form(...)
 ):
-    payload = token_check(request)
-    verification_status = update_user(id=payload["id"], password=forgot_password_obj.password)
-    if verification_status:
-        return True
+    payload = verify_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Token Expired")
     else:
-        raise HTTPException(status_code=400)
+        verification_status = update_user(email=payload["email"], password=password)
+        if verification_status:
+            return True
+        else:
+            raise HTTPException(status_code=400, detail="Email does not exists")
