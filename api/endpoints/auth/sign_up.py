3cd2d473ff9ca3fea59745165570ae0cb05fc8c3
jainal09 <jainal09gosalia@gmail.com>
2020-12-29 09:50:52 +0530

    logout from all devices added

--- api/endpoints/auth/sign_up.py
+++ api/endpoints/auth/sign_up.py
@@ -1,6 +1,4 @@
-from fastapi import Form
 from fastapi import APIRouter, HTTPException, BackgroundTasks
-<<<<<<<< HEAD:api/endpoints/auth/sign_up.py
 from Services.auth.auth_services import sign_up, create_auth_url, check_user,\
     update_user_verification, token_check, create_verify_token
 from Services.mail.mail_service import send_mail
@@ -16,18 +14,11 @@ class SignUpModel(BaseModel):
     last_name: str
     password: str
 
-========
-from Services.auth_services import sign_up, create_auth_url, check_user, get_user_data, \
-    create_access_token, verify_access_token, update_user
-from Services.mail_service import send_mail
-from pydantic import EmailStr
->>>>>>>> 7b1fc9a (removed acm added user auth removed uname from token):api/endpoints/sign_up.py
 
 router = APIRouter()
 
 
 @router.post("/register/", status_code=200)
-<<<<<<<< HEAD:api/endpoints/auth/sign_up.py
 def register(background_tasks: BackgroundTasks, register_obj: SignUpModel):
     username = register_obj.user_name.replace(" ", "").lower()
     user_obj = sign_up(
@@ -49,93 +40,74 @@ def register(background_tasks: BackgroundTasks, register_obj: SignUpModel):
 class VerificationModel(BaseModel):
     type: str
     email: EmailStr
-========
-def register(
-        background_tasks: BackgroundTasks,
-        user_name: str = Form(...),
-        email: EmailStr = Form(...),
-        first_name: str = Form(...),
-        last_name: str = Form(...),
-        password: str = Form(...)
-):
-    token = sign_up(
-        user_name, email, first_name, last_name,password=password 
-    )
-    if token:
-        url = create_auth_url(token, type="verify")
-        data = {"user": user_name, "url": url}
-        background_tasks.add_task(send_mail, [str(email)], "verify", data)
-        return {
-            "access_token": token,
-            "token_type": "bearer",
-        }
-    else:
-        raise HTTPException(status_code=400, detail="User Name or Email does not exists")
->>>>>>>> 7b1fc9a (removed acm added user auth removed uname from token):api/endpoints/sign_up.py
 
 
 @router.post("/resend-verification/", status_code=200)
 def resend(
         background_tasks: BackgroundTasks,
-        type: str = Form(...),
-        email: EmailStr = Form(...),
+        verification_model_obj: VerificationModel
 ):
-    if check_user(email=email):
-        data = get_user_data(email_address=email)
-        token = create_access_token(data=data)
-        if type == "verify":
+    user_obj = check_user(email=verification_model_obj.email)
+    if user_obj:
+        token_data = create_verify_token(user_obj)
+        token = token_data["token"]
+        data = token_data["user_data"]
+        if verification_model_obj.type == "verify":
+            if user_obj.verified:
+                raise HTTPException(
+                    status_code=error_constants.EMAIL_ALREADY_VERIFIED["status_code"],
+                    detail=error_constants.EMAIL_ALREADY_VERIFIED["detail"]
+                )
             url = create_auth_url(token, type="verify")
             data = {"user": data["user_name"], "url": url}
-            background_tasks.add_task(send_mail, [str(email)], "verify", data)
+            background_tasks.add_task(send_mail, [str(verification_model_obj.email)], "verify", data,
+                                      "Verify your Atris Account")
             return True
-        elif type == "forgot_password":
+        elif verification_model_obj.type == "forgot_password":
             url = create_auth_url(token, type="forgot")
             data = {"user": data["user_name"], "url": url}
-            background_tasks.add_task(send_mail, [str(email)], "forgot", data)
+            background_tasks.add_task(send_mail, [str(verification_model_obj.email)], "forgot", data,
+                                      "Reset your Atris Account")
             return True
         else:
-            return HTTPException(status_code=400, detail="Bad Request")
+            return HTTPException(status_code=400)
     else:
-        raise HTTPException(status_code=400, detail="Email does not exists")
+        raise HTTPException(
+            status_code=error_constants.INVALID_EMAIL["status_code"],
+            detail=error_constants.INVALID_EMAIL["detail"]
+        )
 
 
 @router.post("/verify-user/", status_code=200)
 def verification(
-        token: str = Form(...)
+         request: Request
 ):
-    payload = verify_access_token(token)
+    payload = token_check(request)
     if not payload:
-        raise HTTPException(status_code=401, detail="Token Expired")
+        raise HTTPException(
+            status_code=error_constants.TOKEN_EXPIRED["status_code"],
+            detail=error_constants.TOKEN_EXPIRED["detail"]
+        )
     else:
-<<<<<<<< HEAD:api/endpoints/auth/sign_up.py
         verification_status = update_user_verification(id=payload["id"], verified=True)
-========
-        verification_status = update_user(email=payload["email"], verified=True)
->>>>>>>> 7b1fc9a (removed acm added user auth removed uname from token):api/endpoints/sign_up.py
         if verification_status:
             return True
         else:
-            raise HTTPException(status_code=400, detail="Email does not exists")
+            raise HTTPException(status_code=400)
+
+
+class ForgotPasswordModel(BaseModel):
+    password: str
 
 
 @router.post("/forgot-password/", status_code=200)
 def reset(
-        token: str = Form(...),
-        password: str = Form(...)
+        request: Request,
+        forgot_password_obj: ForgotPasswordModel
 ):
-<<<<<<<< HEAD:api/endpoints/auth/sign_up.py
     payload = token_check(request)
     verification_status = update_user_verification(id=payload["id"], password=forgot_password_obj.password)
     if verification_status:
         return True
-========
-    payload = verify_access_token(token)
-    if not payload:
-        raise HTTPException(status_code=401, detail="Token Expired")
->>>>>>>> 7b1fc9a (removed acm added user auth removed uname from token):api/endpoints/sign_up.py
     else:
-        verification_status = update_user(email=payload["email"], password=password)
-        if verification_status:
-            return True
-        else:
-            raise HTTPException(status_code=400, detail="Email does not exists")
+        raise HTTPException(status_code=400)
--- api/endpoints/user_settings.py
+++ api/endpoints/user_settings.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, APIRouter, Request, File, UploadFile
+from fastapi import FastAPI, APIRouter, Request, File, UploadFile, HTTPException
 from settings import MAX_NAME_LENGTH, MIN_NAME_LENGTH
 from pydantic import BaseModel, StrictStr
-from Services.auth.auth_services import token_check, update_user, get_user_data
+from Services.auth.auth_services import token_check, update_user, get_user_data, check_user, remove_ref_token
 from Services.storage_services import upload_profile
 from typing import Optional
-import magic
 
 
 router = APIRouter()
@@ -53,10 +52,23 @@ def change_dp(
         request: Request,
         file: UploadFile = File(...)
 ):
-    email_id = "jainalgosaliya@gmail.com"
     user_dict = token_check(request)
     file_data = file.file.read()
     return upload_profile(email=user_dict["email_id"], file_data=file_data, file_name=file.filename)
+
+
+@router.post("/logout_all_devices", status_code=200)
+def logout_from_all_devices(
+        request: Request
+):
+    user_dict = token_check(request)
+    user_obj = check_user(email=user_dict["email_id"])
+    if not user_obj:
+        raise HTTPException(status_code=400)
+    else:
+        remove_ref_token(user_obj)
+        return True
+
 
 
 
